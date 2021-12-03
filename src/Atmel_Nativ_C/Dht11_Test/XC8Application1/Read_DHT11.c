f5d08c33e3e9da5eac1ac7386911cd2031f5c6e1
Christian Grünewald <christian.gruenewald@ifnano.de>
2021-12-03 17:07:22 +0100

    prepared header and c file for DHT11 reading

---
+++ src/Atmel_Nativ_C/Dht11_Test/XC8Application1/Read_DHT11.c
@@ -0,0 +1,6 @@
+﻿/*
+ * Read_DHT11.c
+ *
+ * Created: 03.12.2021 17:02:31
+ *  Author: Christian Grünewald
+ */ 
---
+++ src/Atmel_Nativ_C/Dht11_Test/XC8Application1/Read_DHT11.h
@@ -0,0 +1,16 @@
+﻿/*
+ * Read_DHT11.h
+ *
+ * Created: 03.12.2021 17:04:52
+ *  Author: Christian Grünewald
+ */ 
+
+
+#ifndef READ_DHT11_H_
+#define READ_DHT11_H_
+
+
+
+
+
+#endif /* READ_DHT11_H_ */
